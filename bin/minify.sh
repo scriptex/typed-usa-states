8370c98574b68ff17936cd2841f0650518b60212
Atanas Atanasov <scriptex.bg@gmail.com>
2022-09-20 12:16:27 +0300

    Update metadata

--- bin/minify.sh
+++ bin/minify.sh
@@ -4,15 +4,5 @@ path="$1"
 
 find $path -name '*.js' -type f | while read f
 do
-    folderpath=$(dirname "$f")
-    filename=$(basename "$f")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    nf=$folderpath/$filename.min.$extension
-
-# ----- METHOD 1 : Replace the old file
     terser "$f" --output "$f" --compress --mangle
-# ----- METHOD 2 : Create .min.js file
-#    terser "$f" --output "$nf" --compress --mangle
-
 done
